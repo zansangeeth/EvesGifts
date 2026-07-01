00c7d28c7612b73238dc65d0be10d7ffd6eb34e0
Sangeeth Amirthanathan <zansangeeth17@gmail.com>
2026-07-01 01:34:40 +0100

    added quotation page title

--- app/src/main/java/com/sangeeth/evesgifts/ui/home/HomeScreen.kt
+++ app/src/main/java/com/sangeeth/evesgifts/ui/home/HomeScreen.kt
@@ -1,12 +1,27 @@
 package com.sangeeth.evesgifts.ui.home
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(){
-    Column {
-        Text("welcome to eve's gifts home page")
+    Column(modifier = Modifier.padding(20.dp)) {
+        Text(
+            text = "Create Quotation",
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            fontSize = 40.sp
+        )
     }
 }
