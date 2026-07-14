9e8f14f700a1462578076781eb3cec82d2c43846
Sangeeth Amirthanathan <zansangeeth17@gmail.com>
2026-07-14 23:34:26 +0100

    implemented logic to loop the frame prices and get the total

--- app/src/main/java/com/sangeeth/evesgifts/utils/PDFGenerator.kt
+++ app/src/main/java/com/sangeeth/evesgifts/utils/PDFGenerator.kt
@@ -14,7 +14,10 @@ import com.sangeeth.evesgifts.data.SelectedCake
 import com.sangeeth.evesgifts.data.SelectedFrame
 import com.sangeeth.evesgifts.data.SelectedGifts
 import android.graphics.*
+import android.icu.text.SimpleDateFormat
 import android.provider.CalendarContract
+import java.util.Date
+
 
 fun PDFGenerator(
     context: Context,
@@ -54,5 +57,54 @@ fun PDFGenerator(
         color = android.graphics.Color.LTGRAY
     }
 
+    var y = 50f
 
+    canvas.drawText("Eve's Gifts", 200f, y, titlePaint)
+
+    y += 30
+    canvas.drawText(
+        "Quotation #: $quotationId",
+        40f,
+        y,
+        textPaint
+    )
+
+    y += 20
+    canvas.drawText(
+        "Customer: $customerName", 40f, y, textPaint
+    )
+
+    y += 20
+    canvas.drawText(
+        "Date: ${SimpleDateFormat("dd/MM/yyyy", java.util.Locale.getDefault()).format(Date())}",
+        40f,
+        y,
+        textPaint
+
+    )
+
+    y += 30
+    canvas.drawLine(40f, y, 555f, y, linePaint)
+
+    y += 20
+
+    canvas.drawText("Item", 40f, y, headerPaint)
+    canvas.drawText("Type", 230f, y, headerPaint)
+    canvas.drawText("QTY", 360f, y, headerPaint)
+    canvas.drawText("Price", 500f, y, headerPaint)
+
+
+    y += 10
+    canvas.drawLine(40f, y, 555f, y, linePaint)
+
+    y += 20
+
+    frames.forEach { frame ->
+        canvas.drawText(frame.category, 40f, y, textPaint)
+        canvas.drawText(frame.size, 230f, y, textPaint)
+        canvas.drawText(frame.quantity.toString(), 360f, y, textPaint)
+
+        val total = (frame.price?.toDouble() ?: 0.0) * frame.quantity
+        canvas.drawText("Rs. %.2f".format(total), 470f, y, textPaint)
+    }
 }
